09a95db409cfe642e0ae345278d75ddbcde45a4d
Matt <sir.matticus@icloud.com>
2021-08-03 17:28:23 -0500

    Updated class to contain various Contact components.

--- Assets/Scripts/GridObjects/Loot.cs
+++ Assets/Scripts/GridObjects/Loot.cs
@@ -5,31 +5,88 @@ using UnityEngine;
 public class Loot : GridObject
 {
     [Header("Loot Item Properties")]
-    [SerializeField] private LootType lootType;
-    [SerializeField] private int lootAmount;
+    //[SerializeField] private LootType lootType;
+    //[SerializeField] private int lootAmount;
 
-    public enum LootType
-    {
-        JumpFuel = 1,
-        MissileAmmo = 2,
-        RailgunAmmo = 3
-    };
+    //[SerializeField]
+    private ContactSupply supply;
 
-    public LootType Type
+    //[SerializeField]
+    private ContactFuel fuel;
+    
+    public WeaponType AmmoType
     {
-        get { return lootType; }
+        get { return supply.weaponType; }
     }
 
-    public int LootAmount
+    public int AmmoAmount
     {
-        get { return lootAmount; }
+        get { return supply.supplyAmount; }
     }
 
-    public void RandomizeLoot()
+    public int FuelAmount
     {
-        int lootSelect = Random.Range(1, 11);
-
-        if (lootSelect <= 5) lootType = LootType.MissileAmmo;
-        else lootType = LootType.RailgunAmmo;
+        get
+        {
+            if (DoesSupplyFuel)
+                return fuel.fuelAmount;
+            else return 0;
+        }   
     }
+
+    public bool DoesSupplyAmmo
+    {
+        get
+        {
+            if (supply != null) return true;
+            else return false;
+        }
+    }
+
+    public bool DoesSupplyFuel
+    {
+        get
+        {
+            if (fuel != null) return true;
+            else return false;
+        }
+    }
+
+    private void Start()
+    {
+        if(TryGetComponent<ContactSupply>(out ContactSupply cs))
+            supply = cs;
+
+        if (TryGetComponent<ContactFuel>(out ContactFuel cf))
+            fuel = cf;
+    }
+    /*
+        public enum LootType
+        {
+            JumpFuel = 1,
+            MissileAmmo = 2,
+            RailgunAmmo = 3
+        };
+    */
+    /*
+        public LootType Type
+        {
+            get { return lootType; }
+        }
+    */
+    /*
+        public int LootAmount
+        {
+            get { return lootAmount; }
+        }
+    */
+    /*
+        public void RandomizeLoot()
+        {
+            int lootSelect = Random.Range(1, 11);
+
+            if (lootSelect <= 5) lootType = LootType.MissileAmmo;
+            else lootType = LootType.RailgunAmmo;
+        }
+    */
 }
