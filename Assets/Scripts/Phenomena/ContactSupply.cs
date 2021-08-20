d5905a6495cace3fd16eb4146a35c601e4a19f40
Matt <sir.matticus@icloud.com>
2021-08-20 09:51:29 -0500

    Staging commit prior to removing comments.

--- Assets/Scripts/Phenomena/ContactSupply.cs
+++ Assets/Scripts/Phenomena/ContactSupply.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class ContactSupply : MonoBehaviour
 {
+    /*
     public enum Type
     {
         AutoCannonAmmo = 1,
         MissileAmmo = 2,
         RailGunAmmo = 3
     }
+    */
 
+    //public Type supplyType;
+    public WeaponType weaponType;
     public int supplyAmount;
 
-    public Type RandomSelect()
+    
+    public void RequestSupply()
     {
-        return (Type)Random.Range(1, 3);
+        //supplyType = (Type)Random.Range(1, 3);
+        weaponType = (WeaponType)Random.Range(1, 3);
+        supplyAmount = SupplyData[weaponType];
     }
+
+/*
+    private Dictionary<Type, int> oldSupplyData = new Dictionary<Type, int>()
+    {
+        {Type.AutoCannonAmmo, 10},
+        {Type.MissileAmmo, 6 },
+        {Type.RailGunAmmo, 2 }
+    };
+*/
+
+    private Dictionary<WeaponType, int> SupplyData = new Dictionary<WeaponType, int>()
+    {
+        { WeaponType.AutoCannon, 10 },
+        { WeaponType.MissileLauncher, 6 },
+        {WeaponType.RailGun, 2 }
+    };
+
+    private void Start()
+    {
+        RequestSupply();
+    }
+
+
 }
