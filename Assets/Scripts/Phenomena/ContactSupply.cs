79b89e603fd2a7a83c51797ff00d89c0d5a83e37
Matt <sir.matticus@icloud.com>
2021-08-20 09:53:54 -0500

    Removed extraneous comments/code.

--- Assets/Scripts/Phenomena/ContactSupply.cs
+++ Assets/Scripts/Phenomena/ContactSupply.cs
@@ -4,35 +4,15 @@ using UnityEngine;
 
 public class ContactSupply : MonoBehaviour
 {
-    /*
-    public enum Type
-    {
-        AutoCannonAmmo = 1,
-        MissileAmmo = 2,
-        RailGunAmmo = 3
-    }
-    */
-
-    //public Type supplyType;
     public WeaponType weaponType;
     public int supplyAmount;
 
     
     public void RequestSupply()
     {
-        //supplyType = (Type)Random.Range(1, 3);
         weaponType = (WeaponType)Random.Range(1, 3);
         supplyAmount = SupplyData[weaponType];
     }
-
-/*
-    private Dictionary<Type, int> oldSupplyData = new Dictionary<Type, int>()
-    {
-        {Type.AutoCannonAmmo, 10},
-        {Type.MissileAmmo, 6 },
-        {Type.RailGunAmmo, 2 }
-    };
-*/
 
     private Dictionary<WeaponType, int> SupplyData = new Dictionary<WeaponType, int>()
     {
@@ -45,6 +25,4 @@ public class ContactSupply : MonoBehaviour
     {
         RequestSupply();
     }
-
-
 }
