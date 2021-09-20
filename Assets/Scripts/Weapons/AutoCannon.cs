77d873c9c53fa205295b97d8ef91412bd19a8f75
Matt <sir.matticus@icloud.com>
2021-09-20 10:21:22 -0500

    2021-09-20 Post-cleansing commit.

--- Assets/Scripts/Weapons/AutoCannon.cs
+++ Assets/Scripts/Weapons/AutoCannon.cs
@@ -17,7 +17,6 @@ public class AutoCannon : Weapon
     {
         ParticleSystem ps = GetComponent<ParticleSystem>();
 
-
         for (int i = 0; i < targetBlock.objectsOnBlock.Count; i++)
         {
             if (targetBlock.objectsOnBlock[i].TryGetComponent<Hazard>(out Hazard hazard))
@@ -35,25 +34,5 @@ public class AutoCannon : Weapon
                 break;
             }
         }
-        /*
-        //foreach (GameObject target in targetBlock.objectsOnBlock)}
-        {
-            Hazard hazardTest = target.GetComponent<Hazard>();
-            if (hazardTest != null)
-            {
-                var trigger = ps.trigger;
-                trigger.enabled = true;
-
-                trigger.SetCollider(0, target.GetComponent<Collider>());
-                trigger.radiusScale = 0.5f;
-
-                ps.Play();
-                yield return new WaitForSeconds(2.0f);
-                ps.Stop();
-
-                break;
-            }
-        }
-        */
     }
 }
