2660132ead1384c951041eac38b0b945d2731da0
Matt <sir.matticus@icloud.com>
2021-09-20 09:58:28 -0500

    2021-09-20 Post-cleansing commit.

--- Assets/Scripts/MovePattern.cs
+++ Assets/Scripts/MovePattern.cs
@@ -9,23 +9,53 @@ public class MovePattern : MonoBehaviour
     [SerializeField] private int waitTicksPerMove;
     public int ticksUntilNextMove;
 
-    public Vector3 DirectionInWorld
-    {
-        get { return new Vector3(delta.x, delta.y, 0.0f); }
-    }
+
     public Vector2Int DirectionOnGrid
     {
         get { return delta; }
     }
     private Vector2Int delta;
 
+    // PROPERTIES: Slow
+    public bool EligibleToSlow { get { return eligibleSlow; } }
+    public bool CanMoveThisTurn
+    {
+        get { return ticksUntilNextMove == 0 && slowTicksRemaining == 0; }
+    }
+    private bool eligibleSlow = true;
+    private int slowTicksRemaining;
+    
 
     private void Awake()
     {
         ticksUntilNextMove = (AlwaysMoveOnSpawn) ? 1 : waitTicksPerMove;        
     }
-    
+    public void OnTickUpdate()
+    {
+        /*  Careful with calls to this method as timing has a great impact
+         *  on GameObjectManager.OnTickUpdate() and 
+         *  GameObjectManager.MoveObjectsForTick()
+         */
+        if (ticksUntilNextMove == 0)
+        {
+            ticksUntilNextMove = waitTicksPerMove;
 
+        }
+        ticksUntilNextMove -= 1;
+
+        if (slowTicksRemaining > 0)
+        {
+            slowTicksRemaining--;
+        }
+        else
+        {
+            slowTicksRemaining = 0;
+            eligibleSlow = true;
+        }
+    }
+    
+    
+    // METHODS: SET DIRECTION
     public Vector2Int SetMovePatternUp()
     {
         delta = Vector2Int.up;
@@ -46,49 +76,13 @@ public class MovePattern : MonoBehaviour
         delta = Vector2Int.right;
         return delta;
     }
-
-
     public void SetMovePattern(Vector2Int directionToMove)
     {
         delta = directionToMove;
     }
 
-    /*  Careful with calls to this method as timing has a great impact
-     *  on GameObjectManager.OnTickUpdate() and 
-     *  GameObjectManager.MoveObjectsForTick()
-     */
-    public void OnTickUpdate()
-    {
-        if(ticksUntilNextMove == 0)
-        {
-            ticksUntilNextMove = waitTicksPerMove;
-            
-        }
-        ticksUntilNextMove -= 1;
-
-        if (slowTicksRemaining > 0)
-        {
-            slowTicksRemaining--;
-        }
-        else
-        {
-            slowTicksRemaining = 0;
-            eligibleSlow = true;
-        }
-    }
-
-    /*
-    public bool CanMoveThisTurn()
-    {
-        return ticksUntilNextMove == 0;
-    }
-    */
-
-    public bool CanMoveThisTurn
-    {
-        get { return ticksUntilNextMove == 0 && slowTicksRemaining == 0; }
-    }
-
+    
+    // METHODS: ContactSlow   
     public void ApplySlow(int slowTickDuration)
     {
         Debug.LogFormat("Delaying Movement of {0} by {1} turns.", this.gameObject.name, slowTickDuration.ToString());
@@ -100,8 +94,4 @@ public class MovePattern : MonoBehaviour
 
         eligibleSlow = false;
     }
-
-    private bool eligibleSlow = true;
-    private int slowTicksRemaining;
-    public bool EligibleToSlow { get { return eligibleSlow; } }
 }
