0646aebf31744f49e22bab4885d10111a841afb5
Matt <sir.matticus@icloud.com>
2021-09-20 09:51:36 -0500

    2021-09-20 Post-cleansing commit.

--- Assets/Scripts/GridBlock.cs
+++ Assets/Scripts/GridBlock.cs
@@ -22,41 +22,7 @@ public class GridBlock : MonoBehaviour
         }
     }
     private GameObject debugRenderPoint;
-
-    public bool IsAvailableForPlayer
-    {
-        get
-        {
-            if (!canSpawn && !DoesContainObjectPreventingPlayerMovement()) return true;
-            else return false;
-        }
-    }
-
-    private bool DoesContainLoot()
-    {
-        int countOfLootItems = 0;
-        for (int i = 0; i < objectsOnBlock.Count; i++)
-        {
-            Loot loot = objectsOnBlock[i].GetComponent<Loot>();
-            if (loot != null) countOfLootItems += 1;
-        }
-        return countOfLootItems > 0;
-    }
-
-    private bool DoesContainObjectPreventingPlayerMovement()
-    {
-        int countOfObjects = 0;
-        for (int i = 0; i < objectsOnBlock.Count; i++)
-        {
-            GridObject gridObject = objectsOnBlock[i].GetComponent<GridObject>();
-            // if(gridObject != null && hazard.hazardType != Hazard.Type.AmmoCrate) countOfObjects += 1;
-            if (gridObject != null)
-            {
-                if (gridObject as SupplyCrate == null) countOfObjects += 1;
-            }
-        }
-        return countOfObjects > 0;
-    }
+    
 
     // CONSTRUCTOR
     public GridBlock(int x, int y)
