789c6daaca88f8b384b74a2485195b76680d2819
Matt <sir.matticus@icloud.com>
2021-10-28 16:52:22 -0500

    New property [startingAmmunition].  Odin changes.

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -14,38 +14,68 @@ public enum WeaponType
 public abstract class Weapon : MonoBehaviour
 {
     virtual public string Name { get { return weaponName; } }
-    [BoxGroup("General Weapon Properties")] public WeaponType weaponType;
-    [BoxGroup("General Weapon Properties")] protected string weaponName; 
-    
-    virtual public int Damage { get { return weaponDamage; } }
-    [BoxGroup("General Weapon Properties")] [SerializeField] protected int weaponDamage = 0;
-    
-    [BoxGroup("General Weapon Properties")] public int weaponAmmunition = 0;
-    
+    [BoxGroup("WEAPON CONFIGURATION", centerLabel:true)] public WeaponType weaponType;
+    [BoxGroup("WEAPON CONFIGURATION")] protected string weaponName; 
+        
+    virtual public int Damage { get { return baseDamage; } }
+    [BoxGroup("WEAPON CONFIGURATION")] [SerializeField] protected int baseDamage;
+    [BoxGroup("WEAPON CONFIGURATION")] [SerializeField] protected float gridDistanceDamageModifier;
+        
     public bool DoesPenetrate { get { return doesPenetrate; } }
-    [BoxGroup("General Weapon Properties")]
-    [SerializeField] protected bool doesPenetrate;
+    [BoxGroup("WEAPON CONFIGURATION")] [SerializeField] protected bool doesPenetrate;
 
-    public bool RequiresGridPlacement { get { return onGridWeaponPrefab != null; } }
-
+    public bool RequiresGridPlacement { get { return requiresGridPlacement; } }
+    [BoxGroup("WEAPON CONFIGURATION")] [SerializeField] protected bool requiresGridPlacement;
+    
     public GameObject WeaponPrefab { get { return onGridWeaponPrefab; } }
-    [BoxGroup("Weapon Assets")] [SerializeField] protected GameObject onGridWeaponPrefab;
-    [BoxGroup("Weapon Assets")] public Sprite weaponIcon;  
+    [BoxGroup("WEAPON ASSETS", centerLabel:true)] public Sprite weaponIcon;
+    [ShowIf("requiresGridPlacement")][BoxGroup("WEAPON ASSETS")] [SerializeField] protected GameObject onGridWeaponPrefab;
 
+    [BoxGroup("WEAPON PROPERTIES")] public int startingAmmunition;
+    [BoxGroup("WEAPON PROPERTIES")] public int maxAmmunition;
+    [BoxGroup("WEAPON PROPERTIES", centerLabel:true)][DisplayAsString] public int currentAmmunition;
+    
+
+    
 
     // METHODS
+    public int CalculateDamage(int gridBlockDistance, bool verboseConsole = false)
+    {
+        /*  NOTE ON gridBlockDistance
+         *    If the Player occupies a GridBlock that is immediately adjacent to the target, the distance = 0
+         *    If there is one GridBlock in between the Player and the target, the distance = 1
+         *    If there are two GridBlocks in between the Player and the target, the distance = 2
+         *    If there are three GridBlocks in between the Player and the target, the distance = 3
+         */
+
+        if (verboseConsole)
+        {
+            Debug.LogFormat("Grid Block Distance received: {0}", gridBlockDistance);
+            Debug.LogFormat("Grid Modifier: {0}", gridDistanceDamageModifier);
+            Debug.LogFormat("Base Damage: {0}", baseDamage);
+        }
+    
+        float modDamageValue = Mathf.Pow((float)gridDistanceDamageModifier, (float)gridBlockDistance);
+        if (verboseConsole)
+            Debug.LogFormat("Damage modifier value: {0}", modDamageValue.ToString());
+        
+        float newDamageValue = baseDamage * modDamageValue;
+        if (verboseConsole)
+            Debug.LogFormat("Damage modifier value: {0}", newDamageValue.ToString());
+        
+        return (int)newDamageValue;
+    }
     public void SubtractAmmo()
     {
-        weaponAmmunition--;
+        currentAmmunition--;
     }  
-    
     public void SupplyAmmo(int ammoAmount)
     {
-        weaponAmmunition += ammoAmount;
+        currentAmmunition += ammoAmount;
     }
 
 
-    // Animation & Coroutines
+    // ANIMATION COROUTINE
     virtual public void StartAnimationCoroutine(GridBlock target)
     {
         StartCoroutine(AnimationCoroutine(target));
